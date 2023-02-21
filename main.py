c4d5621324612994b1fcfd16e546ed4903336365
meluvrose <tanggu3910@gmail.com>
2023-02-21 21:28:57 +0900

    #5.10 Refactor

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
 from requests import get
 from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
 
-base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
-search_term = "react"
+# jobs = extract_wwr_jobs("python")
+# print(jobs)
 
-response = get(f"{base_url}{search_term}")
-if response.status_code != 200:
-    print("Can't request website")
-else:
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find_all("section", class_="jobs")
-    infos_job = []
-    for job_section in jobs:
-        job_posts = job_section.find_all("li")
-        job_posts.pop(-1)
-        for post in job_posts:
-            anchors = post.find_all("a")
-            anchor = anchors[1]
-            link = anchor["href"]
-            company, kind, region = anchor.find_all("span", class_="company")
-            title = anchor.find("span", class_="title")
-            job_data = {
-                "company": company.string,
-                "region": region.string,
-                "position": title.string,
-            }
-            infos_job.append(job_data)
-    # print(result)
-    for info in infos_job:
-        print(info)
+base_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
