65576ce9a5ca144ad7377aa3ef146c65f0247f29
meluvrose <tanggu3910@gmail.com>
2023-02-21 11:34:35 +0900

    #5.6 Job Posts

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ if response.status_code != 200:
     print("Can't request website")
 else:
     soup = BeautifulSoup(response.text, "html.parser")
-    # jobs = soup.find_all('section', {"class": "jobs"})
-    jobs = soup.find_all('section', class_="jobs")
-
-print(soup.find_all)
+    jobs = soup.find_all("section", class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all("li")
+        job_posts.pop(-1)
+        for post in job_posts:
+            print(post)
+            print("\n")
